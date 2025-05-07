df9854380b4cd0679bf8fe4334bb7724be5f324f
dongyiu <wudongyiu135@gmail.com>
2025-05-08 00:03:01 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,7 +156,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': os.environ.get('DB_PORT', '5433'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     },
 }
 
